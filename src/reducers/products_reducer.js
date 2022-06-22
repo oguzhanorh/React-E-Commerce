3df38e982139e01cf18b3930dee615151a5eb1a6
oguzhan.orh1907@gmail.com <oguzhan.orh1907@gmail.com>
2022-06-22 12:15:48 +0300

    product

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -1,3 +1,4 @@
+import { act } from 'react-dom/test-utils';
 import {
   SIDEBAR_OPEN,
   SIDEBAR_CLOSE,
@@ -16,7 +17,23 @@ const products_reducer = (state, action) => {
   if (action.type === SIDEBAR_CLOSE) {
     return { ...state, isSidebarOpen: false };
   }
-
+  if (action.type === GET_PRODUCTS_BEGIN) {
+    return { ...state, products_loading: true };
+  }
+  if (action.type === GET_PRODUCTS_SUCCESS) {
+    const featured_products = action.payload.filter(
+      (product) => product.featured === true
+    );
+    return {
+      ...state,
+      product_loading: false,
+      product: action.payload,
+      featured_products,
+    };
+  }
+  if (action.type === GET_PRODUCTS_ERROR) {
+    return { ...state, products_loading: false, products_error: true };
+  }
   throw new Error(`No Matching "${action.type}" - action type`);
 };
 
