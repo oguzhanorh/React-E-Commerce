e3c9502de982bfb2c6d3d2709f1537b9255449d1
oguzhan.orh1907@gmail.com <oguzhan.orh1907@gmail.com>
2022-06-22 16:47:55 +0300

    Stars finished with automatically

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -2,58 +2,83 @@ import React from 'react';
 import styled from 'styled-components';
 import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs';
 const Stars = ({ stars, reviews }) => {
+  {
+    /* {star} */
+  }
+  {
+    /* {aşağıda ki işlemler manuel bir yaklaşım 1-5 yıldız değerlendirmesi} */
+  }
+  // <span>
+  //   {stars >= 1 ? (
+  //     <BsStarFill />
+  //   ) : stars >= 0.5 ? (
+  //     <BsStarHalf />
+  //   ) : (
+  //     <BsStar />
+  //   )}
+  // </span>
+  // <span>
+  //   {stars >= 2 ? (
+  //     <BsStarFill />
+  //   ) : stars >= 1.5 ? (
+  //     <BsStarHalf />
+  //   ) : (
+  //     <BsStar />
+  //   )}
+  // </span>
+  // <span>
+  //   {stars >= 3 ? (
+  //     <BsStarFill />
+  //   ) : stars >= 2.5 ? (
+  //     <BsStarHalf />
+  //   ) : (
+  //     <BsStar />
+  //   )}
+  // </span>
+  // <span>
+  //   {stars >= 4 ? (
+  //     <BsStarFill />
+  //   ) : stars >= 3.5 ? (
+  //     <BsStarHalf />
+  //   ) : (
+  //     <BsStar />
+  //   )}
+  // </span>
+  // <span>
+  //   {stars === 5 ? (
+  //     <BsStarFill />
+  //   ) : stars >= 4.5 ? (
+  //     <BsStarHalf />
+  //   ) : (
+  //     <BsStar />
+  //   )}
+  // </span>
+  {
+    /* {end of start} */
+  }
+  // <span>
+  //   {stars >= 1 ? <BsStarFill /> : stars >= 0.5 ? <BsStarHalf /> : <BsStar />}
+  // </span>;
+
+  //1-den 5 e kadar bir yıldız değerlendirmesi olacağından böyle bir array tanımlıyoruz.
+  const tempStars = Array.from({ length: 5 }, (_, index) => {
+    const number = index + 0.5; //0.5 olayı bir yıldızın 1.5 - 2.5 3.5 olması durumundan geliyor.
+    return (
+      <span key={index}>
+        {stars >= index + 1 ? (
+          <BsStarFill />
+        ) : stars >= number ? (
+          <BsStarHalf />
+        ) : (
+          <BsStar />
+        )}
+      </span>
+    );
+  });
+
   return (
     <Wrapper>
-      <div className="stars">
-        {/* {star} */}
-        {/* {aşağıda ki işlemler manuel bir yaklaşım 1-5 yıldız değerlendirmesi} */}
-        <span>
-          {stars >= 1 ? (
-            <BsStarFill />
-          ) : stars >= 0.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        <span>
-          {stars >= 2 ? (
-            <BsStarFill />
-          ) : stars >= 1.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        <span>
-          {stars >= 3 ? (
-            <BsStarFill />
-          ) : stars >= 2.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        <span>
-          {stars >= 4 ? (
-            <BsStarFill />
-          ) : stars >= 3.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        <span>
-          {stars === 5 ? (
-            <BsStarFill />
-          ) : stars >= 4.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        {/* {end of start} */}
-      </div>
+      <div className="stars">{tempStars}</div>
       <p className="reviews">({reviews} customer reviews)</p>
     </Wrapper>
   );
