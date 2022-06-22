fc9ae60bf6aaca7164b7de151bedfbb1918c4308
oguzhan.orh1907@gmail.com <oguzhan.orh1907@gmail.com>
2022-06-22 17:08:28 +0300

    AddToCart button(increase and decrease)

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -10,6 +10,27 @@ const AddToCart = ({ product }) => {
 
   const [mainColor, setMainColor] = useState(colors[0]); //direk ürün seçildiğinde seçili gelecek rengi ayarlıyoruz.
 
+  const [amount, setAmount] = useState(1);
+
+  const increaseAmount = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount + 1;
+      if (tempAmount > stock) {
+        tempAmount = stock;
+      }
+      return tempAmount;
+    });
+  };
+  const decreaseAmount = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount - 1;
+      if (tempAmount < 1) {
+        tempAmount = 1;
+      }
+      return tempAmount;
+    });
+  };
+
   return (
     <Wrapper>
       <div className="colors">
@@ -31,7 +52,16 @@ const AddToCart = ({ product }) => {
           })}
         </div>
       </div>
-      <div className="btn-container"> </div>
+      <div className="btn-container">
+        <AmountButtons
+          amount={amount}
+          increaseAmount={increaseAmount}
+          decreaseAmount={decreaseAmount}
+        />
+        <Link to="/cart" className="btn">
+          add to cart
+        </Link>
+      </div>
     </Wrapper>
   );
 };
