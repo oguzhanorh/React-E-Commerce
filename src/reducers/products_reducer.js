3faaa2a555c7dfecdada51fa961e13b855c7a6d0
oguzhan.orh1907@gmail.com <oguzhan.orh1907@gmail.com>
2022-06-22 15:48:04 +0300

    error,featuredproduct,loading,singleproductpage

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -21,6 +21,7 @@ const products_reducer = (state, action) => {
     return { ...state, products_loading: true };
   }
   if (action.type === GET_PRODUCTS_SUCCESS) {
+    //incele sonrasında.
     const featured_products = action.payload.filter(
       (product) => product.featured === true
     );
@@ -34,6 +35,30 @@ const products_reducer = (state, action) => {
   if (action.type === GET_PRODUCTS_ERROR) {
     return { ...state, products_loading: false, products_error: true };
   }
+
+  if (action.type === GET_SINGLE_PRODUCT_BEGIN) {
+    return {
+      ...state,
+      single_product_loading: true,
+      single_product_error: false,
+    };
+  }
+  if (action.type === GET_SINGLE_PRODUCT_SUCCESS) {
+    return {
+      ...state,
+      single_product_loading: false,
+      singleProduct: action.payload,
+    };
+  }
+
+  if (action.type === GET_SINGLE_PRODUCT_ERROR) {
+    return {
+      ...state,
+      single_product_loading: false,
+      single_product_error: true,
+    };
+  }
+
   throw new Error(`No Matching "${action.type}" - action type`);
 };
 
